8df9a8051739d9beccbf8167178988314840bc35
LeCram0ni <marcel.stacha@gmail.com>
2026-09-04 23:11:06 +0200

    filterbuttons ui fix mobile

--- src/components/ui/FilterButton.tsx
+++ src/components/ui/FilterButton.tsx
@@ -13,7 +13,7 @@ export default function FilterButton({ children, value, filterBy, filterHandler 
       const isActive = filterBy === value;
       const baseStyle = `border border-gray-900 px-[0.45rem] 2xl:py-3
        ${isActive ? "bg-accent md:hover:bg-gray-700 md:hover:text-white" : "md:hover:bg-gray-200 bg-white"} 
-       text-gray-900 text-xs sm:text-base min-[400px]:px-4`
+       text-gray-900 text-xs sm:text-base min-[420px]:px-4 max-[350px]:px-1 `
 
       return baseStyle
    }
